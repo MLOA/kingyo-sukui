75df06b294684f3fbb8ca5e24e040c195c2b949f
shisoAqron <nooba704@gmail.com>
2022-08-19 21:14:23 +0900

    fix enemy shape of straight

--- src/enemy03.js
+++ src/enemy03.js
@@ -8,14 +8,26 @@ export class Enemy03 extends Enemy {
   }
 
   draw() {
-    stroke(0, 0, 255);
-    noFill();
+    noStroke();
+    fill(0, 0, 255);
     push();
     translate(this.pos.x, this.pos.y);
     push();
     rotate(this._angle);
-    ellipse(-8, -8, 24, 24);
+    ellipse(-8, -8, 24, 24); // for debug
+    this.polygon(-8, -8, 20, 7);
     pop();
     pop();
+  }
+
+  polygon(x, y, radius, npoints) {
+    const angle = TWO_PI / npoints;
+    beginShape();
+    for (let a = 0; a < TWO_PI; a += angle) {
+      const sx = x + cos(a) * radius;
+      const sy = y + sin(a) * radius;
+      vertex(sx, sy);
+    }
+    endShape(CLOSE);
   }
 }
